89ce946aa2090210a7fdd7b4630d7eeab6e9cda3
sewmin01 <166158378+sewmin01@users.noreply.github.com>
2025-07-07 11:07:58 +0530

    Add files via upload

---
+++ booking DBMS.sql
@@ -0,0 +1,35 @@
+create database booking_management;
+use booking_management;
+CREATE TABLE students (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    department VARCHAR(50)
+);
+
+CREATE TABLE facilities (
+    facility_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    availability BOOLEAN DEFAULT 1
+);
+CREATE TABLE admins (
+    admin_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE bookings (
+    booking_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT,
+    facility_id INT,
+    date DATE NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    purpose TEXT,
+    status VARCHAR(20) DEFAULT 'Pending',
+    FOREIGN KEY (student_id) REFERENCES students(student_id) ON DELETE CASCADE,
+    FOREIGN KEY (facility_id) REFERENCES facilities(facility_id) ON DELETE CASCADE
+);
+
